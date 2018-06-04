fae6d05dd9531fb865b1c9fb26fb7f5c597a3086
macrolad <39697678+macrolad@users.noreply.github.com>
2018-06-04 18:27:19 +0200

    Initial commit

---
+++ Assets/_Scripts/FishScriptable.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Fish", menuName = "Fisherman/Fish", order = 0)]
+public class FishScriptable : ScriptableObject
+{
+    public int ID;
+    public string Name;
+    public Sprite Sprite;
+
+    public enum FishHabitat
+    {
+        Sea,
+        DeepSea,
+        River,
+        Tropical,
+        Artic
+
+    }
+    [Space]
+    [Header("Data")]
+    public FishHabitat Habitat;
+    public float Price;
+    [Space]
+    public float Weight;
+    [Space]
+    public float MinSize;
+    public float MaxSize;
+    [Space]
+    [TextArea(4, 4)]
+    public string Description;
+    [TextArea(1, 2)]
+    public string CatchText;
+
+    [Space]
+    [Header("Parameters")]
+    public float PullForce;
+    public enum FishRarity
+    {
+        VeryCommon,
+        Common,
+        Uncommon,
+        Rare,
+        VeryRare,
+        Mythical
+
+    }
+    public FishRarity Rarity;
+}
